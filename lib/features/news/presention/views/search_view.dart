110a2ea068db88c47bd9672ca2bafaab41dae606
youssef hamada <yosefhagag84171@gmail.com>
2025-04-24 14:33:04 +0200

    add listview to search

--- lib/features/news/presention/views/search_view.dart
+++ lib/features/news/presention/views/search_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:news/core/resources/app_strings.dart';
+import 'package:news/features/news/presention/widgets/custom_list_view.dart';
 import 'package:news/features/news/presention/widgets/search/search_app_bar.dart';
 import 'package:news/features/news/presention/widgets/search/search_bar_widget.dart';
 import 'package:news/features/news/presention/widgets/search/search_history_widget.dart';
+
 import 'package:news/features/news/presention/widgets/search/trending_topics_widget.dart';
 
 class SearchView extends StatelessWidget {
@@ -12,30 +14,33 @@ class SearchView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const SearchAppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SearchBarWidget(),
-              const SizedBox(height: 16.0),
-              const SearchHistoryWidget(),
-              const SizedBox(height: 24.0),
-              const TrendingTopicsWidget(),
-              const SizedBox(height: 24.0),
-              Center(
-                child: Text(
-                  AppStrings.startTypingToSearch,
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                        color: Colors.grey,
-                      ),
+      body: CustomScrollView(slivers: [
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SearchBarWidget(),
+                const SizedBox(height: 16.0),
+                const SearchHistoryWidget(),
+                const SizedBox(height: 24.0),
+                const TrendingTopicsWidget(),
+                const SizedBox(height: 24.0),
+                Center(
+                  child: Text(
+                    AppStrings.startTypingToSearch,
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          color: Colors.grey,
+                        ),
+                  ),
                 ),
-              ),
-            ],
+                const CustomListView()
+              ],
+            ),
           ),
         ),
-      ),
+      ]),
     );
   }
 }
