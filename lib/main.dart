3f8f85e7181367dc8c80a9033774dbd937433d5a
youssef hamada <yosefhagag84171@gmail.com>
2025-04-29 22:58:10 +0300

    delete folder test

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:news/core/resources/app_router.dart';
 import 'package:news/core/theme/app_theme.dart';
+import 'package:news/core/theme/theme_cubit.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  var box = await Hive.openBox('settings');
+  final onboardingSeen = box.get('onboardingSeen', defaultValue: false) as bool;
+  final isDark = box.get('isDarkMode', defaultValue: true) as bool;
+
+  runApp(MyApp(
+    onboardingSeen: onboardingSeen,
+    isDark: isDark,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool onboardingSeen;
+  final bool isDark;
+
+  const MyApp({
+    super.key,
+    required this.onboardingSeen,
+    required this.isDark,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +37,21 @@ class MyApp extends StatelessWidget {
       ),
     );
 
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter().router,
-      theme: AppTheme.darkTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => ThemeCubit()..toggleTheme(isDark)),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, themeMode) {
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            routerConfig: AppRouter(onboardingSeen: onboardingSeen).router,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeMode,
+          );
+        },
+      ),
     );
   }
 }
